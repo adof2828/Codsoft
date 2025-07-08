a5fea5bef5c878bc17e9533e9ef23bdac06faeba
adof2828 <guptapundreek@gmail.com>
2025-07-08 15:32:11 +0530

    Add files via upload
    Landing page

---
+++ loader.js
@@ -0,0 +1,8 @@
+function loadd() {
+    document.getElementById('content').style.display = "none";
+}
+setTimeout(loadd, 5000);
+function loadd2() {
+    document.getElementById('showcontent').style.display="initial";
+}
+setTimeout(loadd2, 5000);
